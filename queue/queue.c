b7291c573754ed214ded482dd45162094f517b42
csjohn <johnkstinson@gmail.com>
2014-07-13 18:38:09 -0400

    Add budget queue implementation using only native c stuff.

--- queue/queue.c
+++ queue/queue.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 typedef struct {
@@ -18,45 +19,76 @@ queue* createQueue(int size)
   return newQueue;
 }
 
-void add(queue* theQueue, float element)
+void display(queue* theQueue)
 {
+  printf("Size: %d, Front: %d, Count: %d. - ", theQueue->size, theQueue->front, theQueue->count);
+  for(int i = theQueue->front; i < theQueue->front + theQueue->count; i++) {
+    printf("%d: %3.2f, ", i, theQueue->data[i % theQueue->size]);
+  }
+  printf("\n");
+}
+
+void enqueue(queue* theQueue, float element)
+{
+  // Grow the data block
   if (theQueue->count >= theQueue->size) {
-    printf("Aint no more room left.\n");
-    return;
+    float* newData = (float*) malloc(sizeof(float) * (theQueue->size * 2));
+    for (int i = 0; i < theQueue->count; i++) {
+      newData[i] = theQueue->data[(theQueue->front + i) % theQueue->size];
+    }
+    free(theQueue->data);
+    theQueue->data = newData;
+    theQueue->front = 0;
+    theQueue->size = theQueue->size * 2;
   }
 
   theQueue->data[(theQueue->front + theQueue->count) % theQueue->size] = element;
   theQueue->count++;
+  printf("Add.   ");
+  display(theQueue);
 }
 
 float dequeue(queue* theQueue)
 {
-  theQueue->count--;
-  return theQueue->data[theQueue->front++];
-}
-
-void display(queue* theQueue)
-{
-  printf("Queue at %p, front: %d, count: %d.\n", theQueue, theQueue->front, theQueue->count);
-  for(int i = theQueue->front; i < theQueue->front + theQueue->count; i++) {
-    printf("Element %d, Value: %f.\n", i, theQueue->data[i % theQueue->size]);
+  if (theQueue->count == 0) {
+    return 0;
   }
+
+  if (theQueue->count < (theQueue->size / 2)) {
+    float* newData = (float*) malloc(sizeof(float) * (theQueue->size / 2));
+    for (int i = 0; i < theQueue->count; i++) {
+      newData[i] = theQueue->data[(theQueue->front + i) % theQueue->size];
+    }
+    free(theQueue->data);
+    theQueue->data = newData;
+    theQueue->front = 0;
+    theQueue->size = theQueue->size / 2;
+  }
+
+  float val = theQueue->data[theQueue->front];
+  theQueue->count--;
+  theQueue->front++;
+  theQueue->front = theQueue->front % theQueue->size;
+  return val;
 }
 
 int main()
 {
-  queue* myQueue = createQueue(5);
-  add(myQueue, 5.0);
-  add(myQueue, 8.0);
-  add(myQueue, 9.0);
-  add(myQueue, 10.0);
-  add(myQueue, 11.0);
-  display(myQueue);
-  dequeue(myQueue);
-  display(myQueue);
-  add(myQueue, 12.0);
-  display(myQueue);
-  dequeue(myQueue);
-  dequeue(myQueue);
-  display(myQueue);
+  queue* myQueue = createQueue(2);
+
+  char input[12];
+  char command[5];
+  float value;
+  while (1) {
+    fgets(input, sizeof(input), stdin);
+    sscanf(input, "%s %f", command, &value);
+
+    if (strcmp("add", strtok(command, "\n")) == 0) {
+      enqueue(myQueue, value);
+    } else if (strcmp("get", strtok(command, "\n")) == 0) {
+      printf("%3.2f\n", dequeue(myQueue));
+    } else if (strcmp("display", strtok(command, "\n")) == 0) {
+      display(myQueue);
+    }
+  }
 }
